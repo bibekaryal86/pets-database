65a1a4098f7b451862603c6484e1639e4efe7b37
Bibek Aryal <bibekaryal86@gmail.com>
2020-12-02 22:13:42 -0700

    Add Spring Security with Basic Authentication (Add Exception for Ping Test)

--- src/main/java/pets/database/config/SpringSecurityConfig.java
+++ src/main/java/pets/database/config/SpringSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static pets.database.utils.Constants.BASIC_AUTH_PWD;
 import static pets.database.utils.Constants.BASIC_AUTH_USR;
@@ -34,7 +35,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity webSecurity) throws Exception {
-        webSecurity.ignoring().antMatchers("/swagger-ui/");
+        webSecurity
+                .ignoring()
+                .antMatchers("/swagger-ui/")
+                .and()
+                .ignoring()
+                .mvcMatchers(GET, "/tests/ping");
     }
 
     @Autowired
